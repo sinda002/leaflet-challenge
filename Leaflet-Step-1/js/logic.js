7394377f7bce04857a08a0f44e0d9dc05b4416b7
Daniel Sinda <sinda002@umn.edu>
2021-07-22 22:11:08 -0500

    Mapbox Key

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -1,13 +1,12 @@
 // Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level for sf
-// This gets inserted into the div with an id of 'map' in index.html
+
 var myMap = L.map("map", {
   center: [37.7749, -122.4194],
   zoom: 5
 });
 
 // Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
+
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
